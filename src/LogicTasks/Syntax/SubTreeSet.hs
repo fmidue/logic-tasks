96d4078acd8cec2dd5d3d104451e34fb6b8c35e7
Patrick Ritzenfeld <patrick.ritzenfeld@stud.uni-due.de>
2022-10-21 13:58:19 +0200

    autotool skeleton for subformula task

--- src/LogicTasks/Syntax/SubTreeSet.hs
+++ src/LogicTasks/Syntax/SubTreeSet.hs
@@ -2,35 +2,61 @@
 
 module LogicTasks.Syntax.SubTreeSet where
 
-import Tasks.SubTree.Config (SubTreeInst(..), SubTreeConfig(..), defaultSubTreeConfig)
-import Tasks.SubTree.Quiz (generateSubTreeInst, feedback)
-import LogicTasks.Syntax.AppHelp (offerChange, determineBaseConfig, feedbackLoop)
-import Test.QuickCheck (generate)
 
-import System.IO (hSetBuffering, stdout, BufferMode(NoBuffering))
-import Text.Pretty.Simple (pPrint)
+import Control.Monad.Output (LangM, OutputMonad(..), english, german, translate)
+import Tasks.SubTree.Config (checkSubTreeConfig, SubTreeInst(..), SubTreeConfig(..))
 
-main :: IO ()
-main = do
-  hSetBuffering stdout NoBuffering
-  theConfigToUse <- determineSubTreeConfig
-  putStrLn "\nThe following is the config now used:\n"
-  pPrint theConfigToUse
-  putStrLn "\nThe following is a random instance generated from it:\n"
-  inst@SubTreeInst{..} <- generate . generateSubTreeInst $ theConfigToUse
-  pPrint inst
-  putStrLn ("\nThe task will give a formula and your task is to input a set which at least have " ++ show minInputTrees ++ " non-atomic Formulas")
-  putStrLn "\nInput form is {subformula1,subformula2..}"
-  putStrLn "\nDo not keep unnecessary parentheses outside subformulas and do not add any additional parentheses"
-  feedbackLoop (feedback inst) ("One of the sample solution is " ++ show correctFormulas)
 
-determineSubTreeConfig :: IO SubTreeConfig
-determineSubTreeConfig = do
-  putStrLn "\nThe following is the default config:\n"
-  pPrint defaultSubTreeConfig
-  let SubTreeConfig{..} = defaultSubTreeConfig
-  syntaxTreeConfig' <- determineBaseConfig syntaxTreeConfig
-  allowSameSubTree' <- offerChange "allowSameSubTree" allowSameSubTree
-  minSubTrees' <- offerChange "minSubTrees" minSubTrees
-  let newConfig = defaultSubTreeConfig {syntaxTreeConfig = syntaxTreeConfig', minSubTrees = minSubTrees', allowSameSubTree = allowSameSubTree'}
-  return newConfig
+
+
+description :: OutputMonad m => SubTreeInst -> LangM m
+description SubTreeInst{..} = do
+    paragraph $ translate $ do
+      german "Betrachten Sie die folgende aussagenlogische Formel:"
+      english "Consider the following propositional logic formula:"
+
+    indent $ code $ formula
+
+    paragraph $ translate $ do
+      german $ "Finden Sie " ++ show minInputTrees ++ " nicht atomare Subformeln, die in dieser Formel enthalten sind."
+      english $ "Find " ++ show minInputTrees ++ " non atomic subformulae that are contained in it."
+
+    paragraph $ translate $ do
+      german "Geben Sie die Lösung als eine Liste der Subformeln an."
+      english "Submit your solution as a  list of subformulae."
+
+    paragraph $ translate $ do
+      german "Entfernen Sie dabei Klammerpaare, die eine Subformel komplett umschließen und fügen Sie keine zusätzlichen Klammern hinzu."
+      english "Remove bracket pairs which only serve to enclose entire Subformulae and do not add any additional brackets."
+
+    paragraph $ translate $ do
+      german "Ist z.B. ~(A \\/ B) die gegebene Formel und es wird eine Subformel gesucht, dann ist die folgende Lösung korrekt:"
+      english "For example, if ~(A \\/ B) is the given formula and one subformula is required, then the solution is:"
+
+    indent $ code "A \\/ B"
+
+
+
+
+verifyInst :: OutputMonad m => SubTreeInst -> LangM m
+verifyInst _ = pure()
+
+
+
+verifyConfig :: OutputMonad m => SubTreeConfig -> LangM m
+verifyConfig = checkSubTreeConfig
+
+
+
+start :: [String]
+start = []
+
+
+
+partialGrade :: OutputMonad m => SubTreeInst -> [Int] -> LangM m
+partialGrade _ _ = pure()
+
+
+
+completeGrade :: OutputMonad m => SubTreeInst -> [Int] -> LangM m
+completeGrade _ _ = pure()
