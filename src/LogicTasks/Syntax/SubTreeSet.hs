4be663c24dbb00325820378bc2107ae15e27affa
Patrick Ritzenfeld <patrick.ritzenfeld@stud.uni-due.de>
2022-11-14 19:28:14 +0100

    fix feedback for subformula task

--- src/LogicTasks/Syntax/SubTreeSet.hs
+++ src/LogicTasks/Syntax/SubTreeSet.hs
@@ -62,7 +62,7 @@ partialGrade _ _ = pure()
 
 completeGrade :: OutputMonad m => SubTreeInst -> [String] -> LangM m
 completeGrade inst sol
-    | not $ feedback inst $ show sol = reject
+    | not $ feedback inst $ concat sol = reject
       "Your solution is not correct."
       "Ihre Abgabe ist nicht die korrekte Lösung"
     | otherwise = pure()
