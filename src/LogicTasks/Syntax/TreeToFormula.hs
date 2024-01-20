0a336f87ae85dd62fc188e5c981145273ab15245
Janis Voigtländer <jvoigtlaender@users.noreply.github.com>
2024-01-20 16:26:29 +0100

    Update TreeToFormula.hs

--- src/LogicTasks/Syntax/TreeToFormula.hs
+++ src/LogicTasks/Syntax/TreeToFormula.hs
@@ -19,7 +19,7 @@ import Data.Digest.Pure.SHA (sha1, showDigest)
 import Data.Maybe (fromJust, isNothing)
 import Image.LaTeX.Render (FormulaOptions(..), SVG, defaultEnv, imageForFormula)
 
-import LogicTasks.Helpers (extra)
+import LogicTasks.Helpers (cacheIO, extra, fullKey, instruct, keyHeading, reject)
 import Tasks.SynTree.Config (checkSynTreeConfig, SynTreeInst(..), SynTreeConfig)
 import Trees.Types (TreeFormulaAnswer(..))
 import Trees.Print (transferToPicture)
