c0d103ca44ee9ba0c0bb6bd8aad177f375510324
Janis Voigtländer <jvoigtlaender@users.noreply.github.com>
2024-02-22 17:41:01 +0100

    Formulierungen

--- src/Tasks/SynTree/Config.hs
+++ src/Tasks/SynTree/Config.hs
@@ -62,13 +62,13 @@ checkSynTreeConfig SynTreeConfig {..}
         german "Maximale Anzahl aufeinander folgender Negationen kann nicht negativ sein."
     | maxConsecutiveNegations == 0 && (even maxNodes || even minNodes) = reject $ do
         english "Syntax tree with no negation cannot have even number of nodes."
-        german "Syntaxbaum ohne Negation kann keine gerade Anzahl Blätter haben."
+        german "Syntaxbaum ohne Negation kann keine gerade Anzahl Knoten haben."
     | minNodes < minDepth = reject$ do
         english "Minimal number of nodes must be at least as big as the minimal depth."
-        german "Minimale Anzahl Blätter muss mindestens so groß wie die minimale Tiefe sein."
+        german "Minimale Anzahl Knoten muss mindestens so groß wie die minimale Tiefe sein."
     | maxNodes < minNodes = reject $ do
         english "Maximal number of nodes must not be smaller than minimal number."
-        german "Maximale Anzahl Blätter ist kleiner als minimale."
+        german "Maximale Anzahl Knoten ist kleiner als minimale."
     | atLeastOccurring < 1 = reject $ do
         english "At least one literal occurs in each formula."
         german "Formel ohne Literale existiert nicht."
@@ -77,16 +77,16 @@ checkSynTreeConfig SynTreeConfig {..}
         german "Anzahl Literale ist zu niedrig für gegebene Einstellungen."
     | minNodes < atLeastOccurring * 2 - 1 = reject $ do
         english "Your minimum number of nodes does not permit enough leaves for all desired literals."
-        german "Minimale Anzahl der Blätter ist zu niedrig um alle Literale zu verwenden."
+        german "Minimale Anzahl der Knoten ist zu niedrig um alle Literale zu verwenden."
     | minDepth < 1 = reject $ do
         english "Minimal depth must be positive"
         german "Minimale Tiefe muss positiv sein."
     | maxDepth < minDepth = reject $ do
-        english "Maximal depth must not be smaller than minimal depth"
-        german "Maximale Tiefe ist kleiner als minimale Tiefe"
+        english "Maximal depth must not be smaller than minimal depth."
+        german "Maximale Tiefe ist kleiner als minimale Tiefe."
     | maxNodes > maxNodesForDepth maxDepth = reject $ do
         english "Your maximum number of nodes is larger than what your maximum depth enables."
-        german "Maximale Anzahl der Blätter würde eingestellte maximale Tiefe verletzen."
+        german "Maximale Anzahl der Knoten würde eingestellte maximale Tiefe verletzen."
     | let maxNodes' = maxNodes - 1
           maxConsecutiveNegations' = maxConsecutiveNegations + 2
           (result, rest) =
@@ -96,16 +96,15 @@ checkSynTreeConfig SynTreeConfig {..}
         english "Your maximum depth value is unreasonably large, given your other settings."
         german "Maximale Tiefe des Baumes ist zu hoch für eingestellte Parameter."
     | minUniqueBinOperators < 0 = reject $ do
-        english "There should be a non-negative number of unique operators"
-        german "Es sollte eine nicht-negative Anzahl an unterschiedlichen Operatoren geben"
+        english "There should be a non-negative number of unique operators."
+        german "Es sollte eine nicht-negative Anzahl an unterschiedlichen Operatoren geben."
     | minUniqueBinOperators > fromIntegral (length [minBound .. maxBound :: BinOp]) = reject $ do
         english "The number of unique operators cannot exceed the maximum number of operators."
         german "Die Anzahl der unterschiedlichen Operatoren kann nicht die maximale Anzahl überschreiten."
     | maxConsecutiveNegations >= maxDepth = reject $ do
-        english "The maximum number of consecutive negations cannot exceed the maximum depth."
-        german "Die maximale Anzahl aufeinanderfolgender Negationen kann die maximale Tiefe nicht überschreiten."
+        english "The maximum number of consecutive negations cannot reach or exceed the maximum depth."
+        german "Die maximale Anzahl aufeinanderfolgender Negationen kann die maximale Tiefe nicht erreichen oder überschreiten."
     | minNodes > maxNodesForDepth minDepth = reject $ do
-        english "Minimum number of nodes does not allow a tree with minimum depth"
-        german "Minimale Anzahl an Blättern ermöglicht keinen Baum mit minimaler Tiefe"
+        english "Minimum number of nodes does not allow a tree with minimum depth."
+        german "Minimale Anzahl an Knoten ermöglicht keinen Baum mit minimaler Tiefe."
     | otherwise = pure()
-
