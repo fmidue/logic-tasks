96107d695208f6af819b42ac176c63c9ae7cbcfe
nimec01 <24428341+nimec01@users.noreply.github.com>
2024-09-12 15:39:32 +0200

    step: save solution in instance

--- src/Config.hs
+++ src/Config.hs
@@ -153,6 +153,7 @@ dDecideInst =  DecideInst
 data StepInst = StepInst {
                  clause1 :: !Clause
                , clause2 :: !Clause
+               , solution :: (Literal, Clause)
                , usesSetNotation :: Bool
                , showSolution :: Bool
                , addText :: Maybe (Map Language String)
@@ -163,6 +164,7 @@ dStepInst :: StepInst
 dStepInst =  StepInst
           { clause1 = mkClause [Not 'A', Not 'C', Literal 'B']
           , clause2 = mkClause [Literal 'A', Not 'C']
+          , solution = (Literal 'A', mkClause [Not 'C', Literal 'B'])
           , usesSetNotation = False
           , showSolution = False
           , addText = Nothing
