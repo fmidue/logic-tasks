1a849342e9af827b39596f8271a5a83a9dc23914
fmi <Katharina>
2024-11-26 20:16:44 +0100

    change PropFormula to SynTree
    (original commit date: Tue Nov 26 20:16:44 2024 +0100)

--- src/FindLiarTask.hs
+++ src/FindLiarTask.hs
@@ -7,7 +7,7 @@ import Data.Text (Text)
 import Data.String.Interpolate (i)
 import Test.QuickCheck.Arbitrary (arbitrary)
 import Test.QuickCheck.Gen
-import Trees.Types (PropFormula(..), BinOp(..))
+import Trees.Types (BinOp(..), SynTree(..))
 
 
 taskData :: Gen ((Char, Bool), (Char, Bool), (Char, Bool), Bool)
@@ -21,44 +21,43 @@ taskData = do
     _
       -> error "This is impossible!"
 
-makeHintsAndFormula :: ((Char, Bool), (Char, Bool), (Char, Bool), Bool) -> ([PropFormula Char], [Text])
+makeHintsAndFormula :: ((Char, Bool), (Char, Bool), (Char, Bool), Bool) -> ([SynTree BinOp Char], [Text])
 makeHintsAndFormula ((xn, xw), (yn, yw), (zn, zw), v) = (parts, hints)
   where
-    xYnOrNotYn = if xw == yw then Atomic yn else Neg (Atomic yn)
-    yZnOrNotZn = if yw == zw then Atomic zn else Neg (Atomic zn)
-    zXnOrNotXn = if v then Atomic xn else Neg (Atomic xn)
-    zYnOrNotYn = if (xw == yw) == v then Neg (Atomic yn) else Atomic yn
+    xYnOrNotYn = if xw == yw then Leaf yn else Not (Leaf yn)
+    yZnOrNotZn = if yw == zw then Leaf zn else Not (Leaf zn)
+    zXnOrNotXn = if v then Leaf xn else Not (Leaf xn)
+    zYnOrNotYn = if (xw == yw) == v then Not (Leaf yn) else Leaf yn
     zOperator :: String
       | zw                                   = " oder"
-      | isNeg zXnOrNotXn == isNeg zYnOrNotYn = " und"
+      | isNot zXnOrNotXn == isNot zYnOrNotYn = " und"
       | otherwise                            = ", aber"
 
     parts = [px, py, pz]
-    px = Assoc Equi (Atomic xn) xYnOrNotYn
-    py = Assoc Equi (Atomic yn) yZnOrNotZn
-    pz = Assoc Equi (Atomic zn) (Brackets (Assoc (if zw then Or else And) zXnOrNotXn zYnOrNotYn))
-    -- formula = foldr1 (Assoc And) (map Brackets parts)
+    px = Binary Equi (Leaf xn) xYnOrNotYn
+    py = Binary Equi (Leaf yn) yZnOrNotZn
+    pz = Binary Equi (Leaf zn) (Binary (if zw then Or else And) zXnOrNotXn zYnOrNotYn)
 
     hints = [hx, hy, hz]
-    hx = [i|#{xn} sagt: "#{yn}#{if isNeg xYnOrNotYn then " lügt"::String else sdW}."|]
-    hy = [i|#{yn} sagt: "#{zn}#{if isNeg yZnOrNotZn then " lügt"::String else sdW}."|]
+    hx = [i|#{xn} sagt: "#{yn}#{if isNot xYnOrNotYn then " lügt"::String else sdW}."|]
+    hy = [i|#{yn} sagt: "#{zn}#{if isNot yZnOrNotZn then " lügt"::String else sdW}."|]
     hz = [i|#{zn} sagt: "#{xn}#{statement}."|]
 
-    isNeg :: PropFormula c -> Bool
-    isNeg (Neg _) = True
-    isNeg _ = False
+    isNot :: SynTree b c -> Bool
+    isNot (Not _) = True
+    isNot _       = False
 
     sdW :: String
     sdW = " sagt die Wahrheit"
 
     statement :: String
-    statement = if isNeg zXnOrNotXn == isNeg zYnOrNotYn
+    statement = if isNot zXnOrNotXn == isNot zYnOrNotYn
       then
-        let truthStatement = if isNeg zXnOrNotXn
+        let truthStatement = if isNot zXnOrNotXn
             then "lügen"::String
             else "sagen die Wahrheit"::String
         in [i|#{zOperator} #{yn} #{truthStatement}|]
       else
-        let partX = if isNeg zXnOrNotXn then " lügt"::String else sdW
-            partY = if isNeg zYnOrNotYn then " lügt"::String else sdW
+        let partX = if isNot zXnOrNotXn then " lügt"::String else sdW
+            partY = if isNot zYnOrNotYn then " lügt"::String else sdW
         in [i|#{partX}#{zOperator} #{yn}#{partY}|]
