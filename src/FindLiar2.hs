de37a588880e054d5c7659b2425daec354836909
fmi <Katharina>
2024-12-24 00:41:02 +0100

    flip C fmidue/ba-katharina-braasch#1
    (original commit date: Tue Dec 24 00:41:02 2024 +0100)

--- src/FindLiar2.hs
+++ src/FindLiar2.hs
@@ -9,17 +9,44 @@ import Trees.Types (BinOp(..), SynTree(..))
 import Trees.Print (simplestDisplay)-- Testing
 
 makeHintsAndFormula :: ((Char, Bool), (Char, Bool), (Char, Bool)) -> ([SynTree BinOp Char], [Text])
-makeHintsAndFormula ((xn, False), (yn, False), (zn, False)) = (parts, hints)
+makeHintsAndFormula ((xn, False), (yn, False), (zn, zw)) = (parts, hints)
   where
+    xZnOrNotZn = if zw then Leaf zn else Not (Leaf zn)
+
+    yZnOrNotZn = if zw then Not (Leaf zn) else Leaf zn
+
+    zXnOrNotXn = if zw then Not (Leaf xn) else Leaf xn
+    zYnOrNotYn = if zw then Not (Leaf yn) else Leaf yn
+
+    xOperator :: String = if isNot (Leaf yn) == isNot xZnOrNotZn then " und" else ", aber"
+    xSdwLgt1 :: String = if xOperator == " und" then "" else sdW
+    xSdwLgt2 :: String
+      | xOperator == " und" = sdW2
+      | otherwise = " lügt"
+
+    yOperator :: String = if isNot (Leaf xn) == isNot yZnOrNotZn then " und" else ", aber"
+    ySdWLgt1 :: String = if xOperator == " und" then sdW else ""
+    ySdWLgt2 :: String
+      | yOperator == " und" = sdW2
+      | otherwise           = " lügt"
+
+    zSdwLgt2 :: String
+      | not zw    = sdW2
+      | otherwise = " lügen"
+
     parts = [px, py, pz]
-    px = Binary Equi (Leaf xn) (Binary And (Leaf yn) (Not (Leaf zn)))
-    py = Binary Equi (Leaf yn) (Binary And (Leaf xn) (Leaf zn))
-    pz = Binary Equi (Leaf zn) (Binary Or (Leaf xn) (Leaf yn))
+    px = Binary Equi (Leaf xn) (Binary And (Leaf yn) xZnOrNotZn)
+    py = Binary Equi (Leaf yn) (Binary And (Leaf xn) yZnOrNotZn)
+    pz = Binary Equi (Leaf zn) (Binary Or zXnOrNotXn zYnOrNotYn)
 
     hints = [hx, hy, hz]
-    hx = [i|#{xn} sagt: "#{yn}#{sdW}, aber #{zn} lügt."|]
-    hy = [i|#{yn} sagt: "#{xn} und #{zn}#{sdW2}."|]
-    hz = [i|#{zn} sagt: "#{xn} oder #{yn}#{sdW2}."|]
+    hx = [i|#{xn} sagt: "#{yn}#{xSdwLgt1}#{xOperator} #{zn}#{xSdwLgt2}."|]
+    hy = [i|#{yn} sagt: "#{xn}#{ySdWLgt1}#{yOperator} #{zn}#{ySdWLgt2}."|]
+    hz = [i|#{zn} sagt: "#{xn} oder #{yn}#{zSdwLgt2}."|]
+
+    isNot :: SynTree b c -> Bool
+    isNot (Not _) = True
+    isNot _       = False
 
     sdW :: String
     sdW = " sagt die Wahrheit"
@@ -32,7 +59,7 @@ makeHintsAndFormula ((_, _), (_, _), (_, _)) = error "not supported yet"
 -- Testing
 main :: IO ()
 main = do
-    let (formulas, hints) = makeHintsAndFormula (('A', False), ('B', False), ('C', False))
+    let (formulas, hints) = makeHintsAndFormula (('A', False), ('B', False), ('C', True))
     putStrLn "\nFormulas:"
     mapM_ (print . simplestDisplay) formulas
     putStrLn "\nHints:"
