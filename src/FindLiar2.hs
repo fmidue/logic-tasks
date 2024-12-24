25a821c4c39294dbdeadde12471baec846572b32
fmi <Katharina>
2024-12-24 06:15:29 +0100

    flip everything fmidue/ba-katharina-braasch#1
    (original commit date: Tue Dec 24 06:15:29 2024 +0100)

--- src/FindLiar2.hs
+++ src/FindLiar2.hs
@@ -9,58 +9,85 @@ import Trees.Types (BinOp(..), SynTree(..))
 import Trees.Print (simplestDisplay)-- Testing
 
 makeHintsAndFormula :: ((Char, Bool), (Char, Bool), (Char, Bool)) -> ([SynTree BinOp Char], [Text])
-makeHintsAndFormula ((xn, False), (yn, False), (zn, zw)) = (parts, hints)
+makeHintsAndFormula ((xn, xw), (yn, yw), (zn, zw)) = (parts, hints)
   where
-    xZnOrNotZn = if zw then Leaf zn else Not (Leaf zn)
+    xYnOrNotYn = if xw == yw then Leaf yn else Not (Leaf yn)
+    xZnOrNotZn = if xw == zw then Not (Leaf zn) else Leaf zn
 
-    yZnOrNotZn = if zw then Not (Leaf zn) else Leaf zn
+    yXnOrNotXn = if xw == yw then Leaf xn else Not (Leaf xn)
+    yZnOrNotZn = if yw == zw then Leaf zn else Not (Leaf zn)
 
-    zXnOrNotXn = if zw then Not (Leaf xn) else Leaf xn
-    zYnOrNotYn = if zw then Not (Leaf yn) else Leaf yn
-
-    xOperator :: String = if isNot (Leaf yn) == isNot xZnOrNotZn then " und" else ", aber"
-    xSdwLgt1 :: String = if xOperator == " und" then "" else sdW
-    xSdwLgt2 :: String
-      | xOperator == " und" = sdW2
-      | otherwise = " lügt"
-
-    yOperator :: String = if isNot (Leaf xn) == isNot yZnOrNotZn then " und" else ", aber"
-    ySdWLgt1 :: String = if xOperator == " und" then sdW else ""
-    ySdWLgt2 :: String
-      | yOperator == " und" = sdW2
-      | otherwise           = " lügt"
-
-    zSdwLgt2 :: String
-      | not zw    = sdW2
-      | otherwise = " lügen"
+    zXnOrNotXn = if xw == zw then Leaf xn else Not (Leaf xn)
+    zYnOrNotYn = if yw == zw then Leaf yn else Not (Leaf yn)
 
     parts = [px, py, pz]
-    px = Binary Equi (Leaf xn) (Binary And (Leaf yn) xZnOrNotZn)
-    py = Binary Equi (Leaf yn) (Binary And (Leaf xn) yZnOrNotZn)
-    pz = Binary Equi (Leaf zn) (Binary Or zXnOrNotXn zYnOrNotYn)
+    px = Binary Equi (Leaf xn) (Binary And xYnOrNotYn xZnOrNotZn)
+    py = Binary Equi (Leaf yn) (Binary And yXnOrNotXn yZnOrNotZn)
+    pz = Binary Equi (Leaf zn) (Binary Or  zXnOrNotXn zYnOrNotYn)
 
-    hints = [hx, hy, hz]
-    hx = [i|#{xn} sagt: "#{yn}#{xSdwLgt1}#{xOperator} #{zn}#{xSdwLgt2}."|]
-    hy = [i|#{yn} sagt: "#{xn}#{ySdWLgt1}#{yOperator} #{zn}#{ySdWLgt2}."|]
-    hz = [i|#{zn} sagt: "#{xn} oder #{yn}#{zSdwLgt2}."|]
+    hints = [hintFromFormula px, hintFromFormula py, hintFromFormula pz]
+
+hintFromFormula :: SynTree BinOp Char -> Text
+hintFromFormula (Binary Equi (Leaf nameA) (Binary operator b c)) =
+  [i|#{nameA} sagt: "#{nameFromLeaf b}#{word1}#{operatorName} #{nameFromLeaf c}#{word2}"|]
+  where
+    operatorName :: String
+      | operator == Or                        = " oder"
+      | operator == And && isNot b == isNot c = " und"
+      | otherwise                             = ", aber"
+
+    word1 :: String
+      | isNot b == isNot c = ""
+      | isNot b            = " lügt"
+      | otherwise          = " sagt die Wahrheit"
+
+    word2 :: String
+      | isNot b && isNot c = " lügen"
+      | isNot b            = " sagt die Wahrheit"
+      |            isNot c = " lügt"
+      | otherwise          = " sagen die Wahrheit"
+
+    nameFromLeaf :: SynTree BinOp Char -> String
+    nameFromLeaf (Leaf name)       = [name]
+    nameFromLeaf (Not (Leaf name)) = [name]
+    nameFromLeaf _                 = error "This is impossible!"
 
     isNot :: SynTree b c -> Bool
     isNot (Not _) = True
     isNot _       = False
 
-    sdW :: String
-    sdW = " sagt die Wahrheit"
+hintFromFormula _ = error "formula not supported"
 
-    sdW2 :: String
-    sdW2 = " sagen die Wahrheit"
-
-makeHintsAndFormula ((_, _), (_, _), (_, _)) = error "not supported yet"
 
 -- Testing
 main :: IO ()
 main = do
-    let (formulas, hints) = makeHintsAndFormula (('A', False), ('B', False), ('C', True))
+    let (formulas, hints) = makeHintsAndFormula (('A', False), ('B', False), ('C', False))
     putStrLn "\nFormulas:"
     mapM_ (print . simplestDisplay) formulas
     putStrLn "\nHints:"
     mapM_ (putStrLn . unpack) hints
+
+    let (formulas2, hints2) = makeHintsAndFormula (('A', False), ('B', False), ('C', True))
+    putStrLn "\nFormulas:"
+    mapM_ (print . simplestDisplay) formulas2
+    putStrLn "\nHints:"
+    mapM_ (putStrLn . unpack) hints2
+
+    let (formulas3, hints3) = makeHintsAndFormula (('A', False), ('B', True), ('C', False))
+    putStrLn "\nFormulas:"
+    mapM_ (print . simplestDisplay) formulas3
+    putStrLn "\nHints:"
+    mapM_ (putStrLn . unpack) hints3
+
+    let (formulas4, hints4) = makeHintsAndFormula (('A', False), ('B', True), ('C', True))
+    putStrLn "\nFormulas:"
+    mapM_ (print . simplestDisplay) formulas4
+    putStrLn "\nHints:"
+    mapM_ (putStrLn . unpack) hints4
+
+    let (formulas5, hints5) = makeHintsAndFormula (('A', True), ('B', False), ('C', False))
+    putStrLn "\nFormulas:"
+    mapM_ (print . simplestDisplay) formulas5
+    putStrLn "\nHints:"
+    mapM_ (putStrLn . unpack) hints5
