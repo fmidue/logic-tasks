dd8523ac417d7480390abe6899f500a7276c7ab1
Janis Voigtländer <jvoigtlaender@users.noreply.github.com>
2024-12-25 07:33:16 +0100

    correct bit flipping (fmidue/ba-katharina-braasch#9)
    * close fmidue/ba-katharina-braasch#1
    
    ---------
    
    Co-authored-by: fmi <Katharina>

--- src/FindLiar2.hs
+++ src/FindLiar2.hs
@@ -10,19 +10,22 @@ import Trees.Types (BinOp(..), SynTree(..))
 makeHintsAndFormula :: ((Char, Bool), (Char, Bool), (Char, Bool)) -> ([SynTree BinOp Char], [Text])
 makeHintsAndFormula ((xn, xw), (yn, yw), (zn, zw)) = (parts, hints)
   where
-    xYnOrNotYn = if xw == yw then Leaf yn else Not (Leaf yn)
+    xOperator  = if xw       then Or            else And
+    xYnOrNotYn = if xw == yw then Leaf yn       else Not (Leaf yn)
     xZnOrNotZn = if xw == zw then Not (Leaf zn) else Leaf zn
 
-    yXnOrNotXn = if xw == yw then Leaf xn else Not (Leaf xn)
-    yZnOrNotZn = if yw == zw then Leaf zn else Not (Leaf zn)
+    yOperator  = if yw       then Or            else And
+    yXnOrNotXn = if yw == xw then Leaf xn       else Not (Leaf xn)
+    yZnOrNotZn = if yw == zw then Leaf zn       else Not (Leaf zn)
 
-    zXnOrNotXn = if xw == zw then Leaf xn else Not (Leaf xn)
-    zYnOrNotYn = if yw == zw then Leaf yn else Not (Leaf yn)
+    zOperator  = if zw       then And           else Or
+    zXnOrNotXn = if zw == xw then Leaf xn       else Not (Leaf xn)
+    zYnOrNotYn = if zw == yw then Leaf yn       else Not (Leaf yn)
 
     parts = [px, py, pz]
-    px = Binary Equi (Leaf xn) (Binary And xYnOrNotYn xZnOrNotZn)
-    py = Binary Equi (Leaf yn) (Binary And yXnOrNotXn yZnOrNotZn)
-    pz = Binary Equi (Leaf zn) (Binary Or  zXnOrNotXn zYnOrNotYn)
+    px = Binary Equi (Leaf xn) (Binary xOperator xYnOrNotYn xZnOrNotZn)
+    py = Binary Equi (Leaf yn) (Binary yOperator yXnOrNotXn yZnOrNotZn)
+    pz = Binary Equi (Leaf zn) (Binary zOperator zXnOrNotXn zYnOrNotYn)
 
     hints = [hintFromFormula px, hintFromFormula py, hintFromFormula pz]
 
