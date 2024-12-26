cfda09846577c319dd93e0a2b7ca516814d83b14
fmi <Katharina>
2024-12-26 07:59:06 +0100

    reuse hintFromFormula fmidue/ba-katharina-braasch#10
    (original commit date: Thu Dec 26 07:59:06 2024 +0100)

--- src/FindLiarTask.hs
+++ src/FindLiarTask.hs
@@ -8,6 +8,7 @@ import Data.String.Interpolate (i)
 import Test.QuickCheck.Arbitrary (arbitrary)
 import Test.QuickCheck.Gen
 import Trees.Types (BinOp(..), SynTree(..))
+import BuildHints (hintFromFormula, isNot)
 
 
 taskData :: Gen ((Char, Bool), (Char, Bool), (Char, Bool), Bool)
@@ -24,14 +25,10 @@ taskData = do
 makeHintsAndFormula :: ((Char, Bool), (Char, Bool), (Char, Bool), Bool) -> ([SynTree BinOp Char], [Text])
 makeHintsAndFormula ((xn, xw), (yn, yw), (zn, zw), v) = (parts, hints)
   where
-    xYnOrNotYn = if xw == yw then Leaf yn else Not (Leaf yn)
-    yZnOrNotZn = if yw == zw then Leaf zn else Not (Leaf zn)
-    zXnOrNotXn = if v then Leaf xn else Not (Leaf xn)
+    xYnOrNotYn = if xw == yw        then Leaf yn       else Not (Leaf yn)
+    yZnOrNotZn = if yw == zw        then Leaf zn       else Not (Leaf zn)
+    zXnOrNotXn = if v               then Leaf xn       else Not (Leaf xn)
     zYnOrNotYn = if (xw == yw) == v then Not (Leaf yn) else Leaf yn
-    zOperator :: String
-      | zw                                   = " oder"
-      | isNot zXnOrNotXn == isNot zYnOrNotYn = " und"
-      | otherwise                            = ", aber"
 
     parts = [px, py, pz]
     px = Binary Equi (Leaf xn) xYnOrNotYn
@@ -39,25 +36,6 @@ makeHintsAndFormula ((xn, xw), (yn, yw), (zn, zw), v) = (parts, hints)
     pz = Binary Equi (Leaf zn) (Binary (if zw then Or else And) zXnOrNotXn zYnOrNotYn)
 
     hints = [hx, hy, hz]
-    hx = [i|#{xn} sagt: "#{yn}#{if isNot xYnOrNotYn then " lügt" else sdW}."|]
-    hy = [i|#{yn} sagt: "#{zn}#{if isNot yZnOrNotZn then " lügt" else sdW}."|]
-    hz = [i|#{zn} sagt: "#{xn}#{statement}."|]
-
-    isNot :: SynTree b c -> Bool
-    isNot (Not _) = True
-    isNot _       = False
-
-    sdW :: String
-    sdW = " sagt die Wahrheit"
-
-    statement :: String
-    statement = if isNot zXnOrNotXn == isNot zYnOrNotYn
-      then
-        let truthStatement :: String = if isNot zXnOrNotXn
-            then "lügen"
-            else "sagen die Wahrheit"
-        in [i|#{zOperator} #{yn} #{truthStatement}|]
-      else
-        let partX = if isNot zXnOrNotXn then " lügt" else sdW
-            partY = if isNot zYnOrNotYn then " lügt" else sdW
-        in [i|#{partX}#{zOperator} #{yn}#{partY}|]
+    hx = [i|#{xn} sagt: "#{yn}#{if isNot xYnOrNotYn then " lügt" else " sagt die Wahrheit"::String}."|]
+    hy = [i|#{yn} sagt: "#{zn}#{if isNot yZnOrNotZn then " lügt" else " sagt die Wahrheit"::String}."|]
+    hz = hintFromFormula pz
