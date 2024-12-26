974ba51e6d49e916a075469289d9cecc94fdd382
fmi <Katharina>
2024-12-26 07:59:06 +0100

    reuse hintFromFormula fmidue/ba-katharina-braasch#10

--- src/FindLiar2.hs
+++ src/FindLiar2.hs
@@ -1,11 +1,10 @@
-{-# language QuasiQuotes #-}
 {-# language OverloadedStrings #-}
 
 module FindLiar2 where
 
 import Data.Text (Text)
-import Data.String.Interpolate (i)
 import Trees.Types (BinOp(..), SynTree(..))
+import BuildHints (hintFromFormula)
 
 makeHintsAndFormula :: ((Char, Bool), (Char, Bool), (Char, Bool)) -> ([SynTree BinOp Char], [Text])
 makeHintsAndFormula ((xn, xw), (yn, yw), (zn, zw)) = (parts, hints)
@@ -28,34 +27,3 @@ makeHintsAndFormula ((xn, xw), (yn, yw), (zn, zw)) = (parts, hints)
     pz = Binary Equi (Leaf zn) (Binary zOperator zXnOrNotXn zYnOrNotYn)
 
     hints = [hintFromFormula px, hintFromFormula py, hintFromFormula pz]
-
-hintFromFormula :: SynTree BinOp Char -> Text
-hintFromFormula (Binary Equi (Leaf nameA) (Binary operator b c)) =
-  [i|#{nameA} sagt: "#{nameFromLeaf b}#{word1}#{operatorName} #{nameFromLeaf c}#{word2}."|]
-  where
-    operatorName :: String
-      | operator == Or                        = " oder"
-      | operator == And && isNot b == isNot c = " und"
-      | otherwise                             = ", aber"
-
-    word1 :: String
-      | isNot b == isNot c = ""
-      | isNot b            = " lügt"
-      | otherwise          = " sagt die Wahrheit"
-
-    word2 :: String
-      | isNot b && isNot c = " lügen"
-      | isNot b            = " sagt die Wahrheit"
-      |            isNot c = " lügt"
-      | otherwise          = " sagen die Wahrheit"
-
-    nameFromLeaf :: SynTree BinOp Char -> String
-    nameFromLeaf (Leaf name)       = [name]
-    nameFromLeaf (Not (Leaf name)) = [name]
-    nameFromLeaf _                 = error "not a Leaf and not a Not Leaf"
-
-    isNot :: SynTree b c -> Bool
-    isNot (Not _) = True
-    isNot _       = False
-
-hintFromFormula _ = error "formula not supported"
