23dec452c9789646ba7e4a154133a6fced7557d0
fmi <streifenh0rnchen@icloud.com>
2025-04-05 16:25:11 +0200

    change protocol type
    (original commit date: Sat Apr 5 16:25:11 2025 +0200)

--- test/HornSpec.hs
+++ test/HornSpec.hs
@@ -13,14 +13,14 @@ spec = do
         , Binary Impl (Leaf '1') (Leaf 'A')
         , Binary Impl (Leaf 'A') (Leaf '0')
         ]) `shouldBe`
-        (False, (2,[(1,'A'),(2,'B')])) -- oder sollte B nicht erst markiert werden?
+        (False, [(1,['A']),(2,['B'])]) -- oder sollte B nicht erst markiert werden?
     it "1->A->B and (A&B)->0" $ do
       findSolution (foldr1 (Binary And)
         [ Binary Impl (Leaf 'A') (Leaf 'B')
         , Binary Impl (Leaf '1') (Leaf 'A')
         , Binary Impl (Binary And (Leaf 'A') (Leaf 'B')) (Leaf '0')
         ]) `shouldBe`
-        (False, (2,[(1,'A'),(2,'B')]))
+        (False, [(1,['A']),(2,['B'])])
     it "1->A->B->C and (A&B&C)->0" $ do
       findSolution (foldr1 (Binary And)
         [ Binary Impl (Leaf 'A') (Leaf 'B')
@@ -28,11 +28,11 @@ spec = do
         , Binary Impl (Leaf 'B') (Leaf 'C')
         , Binary Impl (Binary And (Binary And (Leaf 'A') (Leaf 'B')) (Leaf 'C')) (Leaf '0')
         ]) `shouldBe`
-        (False, (3,[(1,'A'),(2,'B'),(3,'C')]))
+        (False, [(1,['A']),(2,['B']),(3,['C'])])
     it "1->A->B and (B&C)->0" $ do
       findSolution (foldr1 (Binary And)
         [ Binary Impl (Leaf 'A') (Leaf 'B')
         , Binary Impl (Leaf '1') (Leaf 'A')
         , Binary Impl (Binary And (Leaf 'B') (Leaf 'C')) (Leaf '0')
         ]) `shouldBe`
-        (True, (2,[(1,'A'),(2,'B')]))
+        (True, [(1,['A']),(2,['B'])])
