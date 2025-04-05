97dcefce380a9913754b98fd9b819cd7e61ad6c2
fmi <streifenh0rnchen@icloud.com>
2025-04-05 20:43:08 +0200

    change alg type to match submission
    (original commit date: Sat Apr 5 20:43:08 2025 +0200)

--- test/HornSpec.hs
+++ test/HornSpec.hs
@@ -1,38 +1,38 @@
 module HornSpec (spec) where
 
 import Test.Hspec
-import Horn (findSolution)
+import Horn (startAlgorithm)
 import Trees.Types (BinOp(..), SynTree(..))
 
 spec :: Spec
 spec = do
   describe "Markierungsalgorithmus" $ do
     it "1->A->B and A->0" $ do
-      findSolution (foldr1 (Binary And)
+      startAlgorithm (foldr1 (Binary And)
         [ Binary Impl (Leaf 'A') (Leaf 'B')
         , Binary Impl (Leaf '1') (Leaf 'A')
         , Binary Impl (Leaf 'A') (Leaf '0')
         ]) `shouldBe`
-        (False, [(1,['A']),(2,['B'])]) -- oder sollte B nicht erst markiert werden?
+        ([(1,['A']),(2,['B'])], False, []) -- oder sollte B nicht erst markiert werden?
     it "1->A->B and (A&B)->0" $ do
-      findSolution (foldr1 (Binary And)
+      startAlgorithm (foldr1 (Binary And)
         [ Binary Impl (Leaf 'A') (Leaf 'B')
         , Binary Impl (Leaf '1') (Leaf 'A')
         , Binary Impl (Binary And (Leaf 'A') (Leaf 'B')) (Leaf '0')
         ]) `shouldBe`
-        (False, [(1,['A']),(2,['B'])])
+        ([(1,['A']),(2,['B'])], False, [])
     it "1->A->B->C and (A&B&C)->0" $ do
-      findSolution (foldr1 (Binary And)
+      startAlgorithm (foldr1 (Binary And)
         [ Binary Impl (Leaf 'A') (Leaf 'B')
         , Binary Impl (Leaf '1') (Leaf 'A')
         , Binary Impl (Leaf 'B') (Leaf 'C')
         , Binary Impl (Binary And (Binary And (Leaf 'A') (Leaf 'B')) (Leaf 'C')) (Leaf '0')
         ]) `shouldBe`
-        (False, [(1,['A']),(2,['B']),(3,['C'])])
+        ([(1,['A']),(2,['B']),(3,['C'])], False, [])
     it "1->A->B and (B&C)->0" $ do
-      findSolution (foldr1 (Binary And)
+      startAlgorithm (foldr1 (Binary And)
         [ Binary Impl (Leaf 'A') (Leaf 'B')
         , Binary Impl (Leaf '1') (Leaf 'A')
         , Binary Impl (Binary And (Leaf 'B') (Leaf 'C')) (Leaf '0')
         ]) `shouldBe`
-        (True, [(1,['A']),(2,['B'])])
+        ([(1,['A']),(2,['B'])], True, [('A', True),('B', True),('C', False)])
