9160933df6cdfbdd8937aefe076a2200400ddb22
fmi <streifenh0rnchen@icloud.com>
2025-06-10 19:15:57 +0200

    fun for alternative solutions fmidue/ba-katharina-braasch#27
    (original commit date: Tue Jun 10 19:15:57 2025 +0200)

--- test/HornSpec.hs
+++ test/HornSpec.hs
@@ -1,8 +1,9 @@
 module HornSpec (spec) where
 
 import Test.Hspec
-import Horn (startAlgorithm)
+import Horn
 import Trees.Types (BinOp(..), SynTree(..))
+
 
 spec :: Spec
 spec = do
@@ -36,3 +37,35 @@ spec = do
         , Binary Impl (Binary And (Leaf 'B') (Leaf 'C')) (Leaf '0')
         ]) `shouldBe`
         ([(1,['A']),(2,['B'])], True, [('A', True),('B', True),('C', False)])
+
+  describe "Check Different Solution" $ do
+    it "correct order, using a shortcut" $ do
+      newSolution (foldr1 (Binary And) v3) [(1,"BA"),(2,"D"),(3,"E")]
+      `shouldBe`
+      ([(1,"BA"),(2,"D"),(3,"E")],False,[])
+
+    it "different but correct order of marks" $ do
+      newSolution (foldr1 (Binary And) v3) [(1,"BA"),(2,"D"),(3,"C"),(4,"E")]
+      `shouldBe`
+      ([(1,"BA"),(2,"D"),(3,"C"),(4,"E")],False,[])
+
+    it "illegal marking of E without marking D" $ do
+      newSolution (foldr1 (Binary And) v3) [(1,"BA"),(2,"C"),(3,"E")]
+      `shouldBe`
+      ([(1,"AB"),(2,"C"),(3,"D"),(4,"E")],False,[])
+
+
+    it "correct order, missing marks" $ do
+      newSolution (foldr1 (Binary And) v4) [(1,"BA"),(2,"D"),(3,"E")]
+      `shouldBe`
+      ([(1,"BA"),(2,"D"),(3,"E"),(4,"C")],True,[('B',True),('A',True),('D',True),('E',True),('C',True),('F',False)])
+
+    it "different but correct order of marks" $ do
+      newSolution (foldr1 (Binary And) v4) [(1,"BA"),(2,"D"),(3,"E"),(4,"C")]
+      `shouldBe`
+      ([(1,"BA"),(2,"D"),(3,"E"),(4,"C")],True,[('B',True),('A',True),('D',True),('E',True),('C',True),('F',False)])
+
+    it "illegal marking of E without marking D" $ do
+      newSolution (foldr1 (Binary And) v4) [(1,"BA"),(2,"C"),(3,"E")]
+      `shouldBe`
+      ([(1,"AB"),(2,"C"),(3,"D"),(4,"E")],True,[('A',True),('B',True),('C',True),('D',True),('E',True),('F',False)])
