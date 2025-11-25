07e5c34c0f509426b7e8937fc7b4230affacd164
fmi <streifenh0rnchen@icloud.com>
2025-11-25 07:50:05 +0100

    Umformulierung der Aufgabenstellung

--- src/LogicTasks/Syntax/SimplestFormula.hs
+++ src/LogicTasks/Syntax/SimplestFormula.hs
@@ -76,20 +76,16 @@ description SuperfluousBracketsInst{..} = do
         when showArrowOperators arrowsKey
 
         instruct $ do
-          english "Since ∧ and ∨ are associative, it is not necessary to use brackets in subformulas with three or more atomic formulas and the same logical operators, for example in:"
-          german "Aufgrund der Assoziativität von ∧ und ∨ muss in Teilformeln mit drei oder mehr atomaren Formeln und den gleichen logischen Operatoren nicht geklammert werden, z.B. bei:"
+          english "Due to the associativity of ∧ and ∨, it is not necessary to use brackets in formulas with three or more subformulas that are connected exclusively by the same logical operator, for example in:"
+          german "Aufgrund der Assoziativität von ∧ und ∨ muss in Formeln mit drei oder mehr Teilformeln, welche ausschließlich durch den gleichen logischen Operatoren verknüpft sind, nicht geklammert werden, z.B. bei:"
 
-        focus "A ∧ B ∧ C"
+        focus "A ∧ B ∧ (C ∨ D ∨ E)"
 
         instruct $ do
           english "Similarly, brackets are not necessary for one or more consecutive negations directly in front of an atomic formula, for example in:"
           german "Genauso sind Klammern bei einer oder mehreren Negationen direkt vor einer atomaren Formel nicht nötig, z.B. bei"
 
         focus "¬¬A"
-
-        instruct $ do
-          english "Remove all unnecessary pairs of brackets in the given formula (regarding associativity not just concerning atomic formulas)."
-          german "Entfernen Sie alle unnötigen Klammer-Paare in der gegebenen Formel (hinsichtlich Assoziativität nicht nur atomare Formeln betreffend)."
 
         pure ()
       )
