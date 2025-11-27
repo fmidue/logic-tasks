65feb068e4f47d693ce00c8673814bc346b9607f
Patrick Ritzenfeld <patrick.ritzenfeld@uni-due.de>
2025-11-27 15:32:21 +0100

    Generalize resolution task description for multiple inputs (#327)
    * attempt to make resolution task description more generally fitting
    
    * "used clauses" -> "clauses used"
    
    (A native English speaker once told me to avoid saying "used X", because it has the negative connotation that comes from "used cars".)
    
    ---------
    
    Co-authored-by: Janis Voigtländer <jvoigtlaender@users.noreply.github.com>

--- src/LogicTasks/Semantics/Resolve.hs
+++ src/LogicTasks/Semantics/Resolve.hs
@@ -140,9 +140,11 @@ descriptionMultipleFields :: OutputCapable m => State (Map Language String) () -
 descriptionMultipleFields explainEmptySteps resInst@ResolutionInst{..} = baseDescription
     (do
       german "Geben Sie die Lösung als eine Auflistung von Schritten an. "
-      german "Füllen Sie für jeden Schritt die zugehörigen drei Eingabefelder mit den zwei verwendeten Klauseln sowie der daraus entstehenden Resolvente. "
+      german "Ein Schritt besteht aus den zwei verwendeten Klauseln sowie der daraus entstehenden Resolvente."
+      german "Füllen Sie für jeden Schritt die leeren Eingabefelder aus. "
       english "Provide the solution as a sequence of steps. "
-      english "Fill the three input fields for each step with the two used clauses and the resulting resolvent. "
+      english "A step consists of the two clauses used and the resulting resolvent."
+      english "Fill in the empty input fields for each step. "
       explainEmptySteps
     )
     (do
