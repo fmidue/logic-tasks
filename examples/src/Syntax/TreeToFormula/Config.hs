4f9e8094117bb4fa1634f7cec4febdad02ffaa3f
fmi <streifenh0rnchen@icloud.com>
2026-01-08 07:50:25 +0100

    fit hints in TreeToFormula example config to task

--- examples/src/Syntax/TreeToFormula/Config.hs
+++ examples/src/Syntax/TreeToFormula/Config.hs
@@ -39,12 +39,8 @@ task02 = TreeToFormulaConfig
     , minUniqueBinOperators = 2
     }
   , extraText = Just (listToFM
-                        [ (German, "Es muss die exakte Formel des Syntaxbaums angegeben werden. "
-                                ++ "Andere, selbst zu dieser Formel semantisch äquivalente Formeln sind keine korrekte Lösung! "
-                                ++ "Sie dürfen bei dieser Aufgabe nicht Klammern durch Verwendung von Assoziativität weglassen.")
-                        , (English, "The exact formula of the syntax tree must be given. "
-                                ++ "Other formulas that are semantically equivalent to this formula are incorrect solutions! "
-                                ++ "Do not try to use associativity in order to omit brackets in this task.")
+                        [ (German, "Sie dürfen bei dieser Aufgabe nicht Klammern durch Verwendung von Assoziativität weglassen.")
+                        , (English, "Do not try to use associativity in order to omit brackets in this task.")
                         ])
   , printSolution = True
   , offerUnicodeInput = True
