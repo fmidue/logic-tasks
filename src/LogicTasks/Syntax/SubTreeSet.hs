37eb46f259840aedce1f871cb32b8e8db8a82cfa
Janis Voigtländer <jvoigtlaender@users.noreply.github.com>
2026-06-21 20:06:05 +0200

    formulation

--- src/LogicTasks/Syntax/SubTreeSet.hs
+++ src/LogicTasks/Syntax/SubTreeSet.hs
@@ -72,8 +72,8 @@ description withListInput SubTreeInst{..} = do
       pure ()
 
     paragraph $ translate $ do
-      german "Sie können dafür die ursprüngliche Formel mehrfach in die Abgabe kopieren und Teile entfernen, oder leer startend die folgenden Schreibweisen nutzen:"
-      english "You can copy the original formula into the submission several times and remove parts, or start from scratch and use the following syntax:"
+      german "Sie können dafür die ursprüngliche Formel mehrfach in die Abgabe kopieren und Teile entfernen oder leer starten."
+      english "You can copy the original formula into the submission several times and remove parts or start from scratch."
 
     keyHeading
     basicOpKey unicodeAllowed
